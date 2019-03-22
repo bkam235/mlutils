da7614007bc35c4969112746541385e5c41cbddf
bkam235 <benedikt@henglarn.de>
2019-03-22 08:30:07 +0100

    Added target encoding

--- R/encode_target.R
+++ R/encode_target.R
@@ -6,18 +6,23 @@
 #' @import glue
 #'
 #' @param data data.table with features and target variable
+#' @param val_data data.table of validation data with features
 #' @param vars_to_encode features to encode
 #' @param target_var target variable to encode features with
 #' @param n_folds number of folds across which encodings are calculated
 #' @param alpha smoothing parameter for encoding
 
 encode_target <- function(data,
+                          val_data = NULL,
                           vars_to_encode = colnames(data)[-match(target_var, colnames(data))],
                           target_var,
                           n_folds = 5,
                           alpha = 0.1){
+  encoding <- NULL
+  t_mean <- NULL
 
   data <- data[, (c(vars_to_encode, target_var)), with = F]
+  val_data <- val_data[, (vars_to_encode), with = F]
   data[[target_var]] <- as.numeric(data[[target_var]])
   folds <- caret::createFolds(y = data[[target_var]], k = n_folds)
 
@@ -29,11 +34,36 @@ encode_target <- function(data,
 
       mean_target[, encoding := (t_mean * n + t_mean_global * alpha) / (n + alpha)]
 
+
       data[test_idx, eval(glue::glue("{var}_tmean"))] <- mean_target$encoding[match(data[test_idx, ][[var]], mean_target[[var]])]
+
+      # var can be unique, this results in NAs in encoding
+      # replace NAs with target means of closest var values
+
+      #Add global mean
+      data[test_idx, ][is.na(get(glue::glue("{var}_tmean"))), eval(glue::glue("{var}_tmean"))] <- mean(mean_target$encoding)
     }
-    data[[var]] <- NULL
   }
-  data[[target_var]] <- NULL
-  return(data)
+
+  if(is.null(val_data) == FALSE){
+    mean_target <- data[, list(t_mean = mean(get(target_var)), n = .N), by = eval(var)]
+    t_mean_global <- data[, mean(get(target_var))]
+    mean_target[, t_mean_global := t_mean_global]
+    mean_target[, encoding := (t_mean * n + t_mean_global * alpha) / (n + alpha)]
+
+    val_data[, eval(glue::glue("{var}_tmean"))] <- mean_target$encoding[match(val_data[[var]], mean_target[[var]])]
+
+    val_data[is.na(get(glue::glue("{var}_tmean"))), eval(glue::glue("{var}_tmean"))] <- mean(mean_target$encoding)
+
+    data[[target_var]] <- NULL
+    data[, (vars_to_encode) := NULL]
+    val_data[[var]] <- NULL
+
+  } else {
+    data[[target_var]] <- NULL
+    data[, (vars_to_encode) := NULL]
+  }
+
+  return(list(data, val_data))
 }
 
--- tests/testthat/test_encode_target.R
+++ tests/testthat/test_encode_target.R
@@ -8,7 +8,14 @@ enc <- encode_target(data = dt_iris,
                      vars_to_encode = "Sepal.Length",
                      target_var = "Species",
                      n_folds = 3,
-                     alpha = 0.1)
+                     alpha = 0.1)[[1]]
+
+enc2 <- encode_target(data = dt_iris[1:100, ],
+                      val_data = dt_iris[101:150, -"Species"],
+                      vars_to_encode = "Sepal.Length",
+                      target_var = "Species",
+                      n_folds = 3,
+                      alpha = 0.1)
 
 test_that("Returns only encoded features", {
   expect_true(colnames(enc) == "Sepal.Length_tmean")
@@ -20,3 +27,24 @@ test_that("Encoding calculated correctly", {
   test <- round(enc$Sepal.Length_tmean[1:5], 14)
   expect_true(all(check == test))
 })
+
+test_that("Encoding does not create NAs", {
+  expect_true(all(is.na(enc$Sepal.Length_tmean)) == FALSE)
+})
+
+test_that("Val set encoding returns list of 2", {
+  expect_true(length(enc2) == 2)
+})
+
+test_that("Val set encoding does not create NAs", {
+  expect_true(all(is.na(enc2[[2]]$Sepal.Length_tmean)) == FALSE)
+})
+
+test_that("Val set encoding calculated correctly", {
+  check <- c(1.98387096774194, 1.74390243902439, 1.57048915546685, 1.98387096774194,
+             1.95454545454545)
+  test <- round(enc2[[2]]$Sepal.Length_tmean[1:5], 14)
+  expect_true(all(check == test))
+})
+
+
